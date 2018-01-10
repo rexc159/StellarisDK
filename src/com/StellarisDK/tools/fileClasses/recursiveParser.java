1c7b24ea26e25f1e9f839975c983c69a4687d5c0
Rex Chan <rexc159@gmail.com>
2018-01-09 22:13:01 -0800

    Data Mapping Test 2 RegEx matching seems to be correct

--- src/com/StellarisDK/tools/fileClasses/recursiveParser.java
+++ src/com/StellarisDK/tools/fileClasses/recursiveParser.java
@@ -5,11 +5,11 @@ import java.util.regex.Pattern;
 
 public class recursiveParser {
 
-    private final static Pattern object = Pattern.compile("(?s)(?m)(^\\w+) = \\{(.+?)\\n\\}\\n?");
-    private final static Pattern constant = Pattern.compile("(\\@\\w+) = (.+)");
-    private final static Pattern type1 = Pattern.compile("(?s)(?m)^\\t(\\w+) = (\\{\\s*[^\n]+?)\\}");
-    private final static Pattern type2 = Pattern.compile("(?s)(?m)^\\t(\\w+) = (\\{[\\r\\n].+?)^\t\\}");
-    private final static Pattern kv = Pattern.compile("(?m)^\\t(\\w+) = ([^\\{#\\n]+)(#.+)*");
+//    private final static Pattern object = Pattern.compile("(?s)(?m)(^\\w+) = \\{(.+?)\\n\\}\\n?");
+//    private final static Pattern constant = Pattern.compile("(\\@\\w+) = (.+)");
+    private final static Pattern type1 = Pattern.compile("(?m)^\\t(\\w+) = \\{\\s*(.+?)\\}");
+    private final static Pattern type2 = Pattern.compile("(?s)(?m)^\\t(\\w+) = \\{[\\r\\n](.+?)^\t\\}");
+    private final static Pattern kv = Pattern.compile("(?m)^\\t?(\\w+) . ([^\\{#\\n]+)(#.+)*");
 
     public static void parse(String input){
 
@@ -19,11 +19,12 @@ public class recursiveParser {
         Matcher t2_match = type2.matcher(input);
 
         while(kv_match.find()){
-            System.out.println(kv_match.group(1).trim()+":"+kv_match.group(2).trim());
+            System.out.println(kv_match.group(1).trim()+":!"+kv_match.group(2).trim());
         }
 
         while(t1_match.find()){
-            System.out.println(t1_match.group(1).trim()+":"+t1_match.group(2).trim());
+            System.out.println(t1_match.group(1).trim()+":{");
+            parse(t1_match.group(2).replaceAll("(?m)^\t",""));
         }
         while(t2_match.find()){
             System.out.println("\t"+t2_match.group(1).trim()+":");
